cb370b12c49460b1367b91f2d045169ae3c1aaf4
Jugurta12 <jugurta.abdessmed@icloud.com>
2026-01-24 16:40:07 +0100

    add js effect

--- script.js
+++ script.js
@@ -41,3 +41,20 @@ links.forEach(link => {
     }
   });
 });
+
+
+
+const slideElements = document.querySelectorAll('.desc, .lien, .nom');
+
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) { 
+      entry.target.classList.add('active');
+      observer.unobserve(entry.target); 
+    }
+  });
+}, { threshold: 0.1 }); 
+
+// Observer chaque élément
+slideElements.forEach(el => observer.observe(el));
